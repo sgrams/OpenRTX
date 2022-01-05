35dca6f4f4faaf4773c00ccb1c4f1d9ad7c25c5a
Silvano Seva <silseva@fastwebnet.it>
2022-01-05 21:47:30 +0100

    Initial minimal support for Ailunce HD-1, just boot code and LED control

---
+++ platform/drivers/display/display_HD1.c
@@ -0,0 +1,59 @@
+/***************************************************************************
+ *   Copyright (C) 2022 by Federico Amedeo Izzo IU2NUO,                    *
+ *                         Niccolò Izzo IU2KIN                             *
+ *                         Frederik Saraci IU2NRO                          *
+ *                         Silvano Seva IU2KWO                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#include <interfaces/display.h>
+#include <stddef.h>
+
+void display_init()
+{
+}
+
+void display_terminate()
+{
+}
+
+void display_renderRow(uint8_t row)
+{
+}
+
+void display_renderRows(uint8_t startRow, uint8_t endRow)
+{
+
+}
+
+void display_render()
+{
+
+}
+
+bool display_renderingInProgress()
+{
+    return false;
+}
+
+void *display_getFrameBuffer()
+{
+    return NULL;
+}
+
+void display_setContrast(uint8_t contrast)
+{
+
+}
